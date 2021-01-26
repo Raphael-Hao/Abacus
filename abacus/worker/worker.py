93e1b9c20cd11ca24c9e09e1a57a9c3cc1884ab1
Raphael-Hao <raphaelhao@outlook.com>
2021-01-26 16:25:51 +0800

    update

--- abacus/worker/worker.py
+++ abacus/worker/worker.py
@@ -131,4 +131,20 @@ class ServerWorker(AbacusWorker):
         self._barrier = barrier
 
     def run(self) -> None:
-        return super().run()
+        timestamp("Server Woker for {}".format(self._model_name), "Starting")
+        os.environ["CUDA_MPS_PIPE_DIRECTORY"] = "/tmp/nvidia-mps"
+        os.environ["CUDA_MPS_LOG_DIRECTORY"] = "/tmp/nvidia-log"
+        os.environ["CUDA_MPS_ACTIVE_THREAD_PERCENTAGE"] = "100"
+        torch.device("cuda")
+        torch.backends.cudnn.enabled = True
+        if self._model_name == "inception_v3":
+            self._inputs = {
+                k: torch.rand(k, 3, 299, 299).half().cuda()
+                for k in self._supported_batchsize
+            }
+        else:
+            self._inputs = {
+                k: torch.rand(k, 3, 224, 224).half().cuda()
+                for k in self._supported_batchsize
+            }
+        self._model = self._model_func().half().cuda().eval()
