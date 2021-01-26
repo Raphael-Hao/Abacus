a0b990783666523f8ea3ab664c70f9f75a8afb86
Raphael-Hao <raphaelhao@outlook.com>
2021-01-26 22:07:47 +0800

    update

--- abacus/server.py
+++ abacus/server.py
@@ -1,15 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # Author: raphael hao
+import os
+import datetime
+import torch
+import torch.multiprocessing as mp
+from torch.multiprocessing import Process
 
+from abacus.option import RunConfig
+from abacus.utils import timestamp
 from abacus.worker import ServerWorker
+from abacus.modeling.models import MLPregression
 
-class AbacusServer():
-    def __init__(self) -> None:
-        pass
+
+class Scheduler(Process):
+    def __init__(self, models, predictor_path, barrier, queues, pipes) -> None:
+        self._predictor = MLPregression()
+        self._predictor.load_state_dict(torch.load(predictor_path))
+        self._barrier = barrier
+        self._queues = queues
+        self._pipes = pipes
+
+    def run(self) -> None:
+        timestamp("Scheduler", "Ready")
+        self._scheduling_queries = {}
+        while True:
+            pass
+
+class AbacusServer:
+    def __init__(self, run_config: RunConfig) -> None:
+        self._run_config = run_config
+        self._barrier = mp.Barrier(run_config.total_models + 1)
+        self._workers = {}
+        self._queues = {}
+        self._pipes = {}
+
     def send_query(self, model_name, batch_size, seq_length):
-        pass
-    def set_config():
-        pass
-    def start_up():
-        pass
+        query_start = datetime.datetime.now()
+        self._queues[model_name].put((model_name, batch_size, seq_length, query_start))
+
+    def start_up(self):
+        timestamp("All Server Workers", "Initializing")
+        for model_id in self._run_config.serve_combination:
+            model_name = self._run_config.models_name[model_id]
+            pipe_parent, pipe_child = mp.Pipe()
+            model_worker = ServerWorker(
+                self._run_config,
+                model_name,
+                self._run_config.supported_batchsize,
+                self._run_config.supported_seqlen,
+                pipe_child,
+                self._barrier,
+            )
+            model_worker.start()
+            self._workers[model_name] = model_worker
+            self._queues[model_name] = mp.Queue()
+            self._pipes[model_name] = pipe_parent
+        self._barrier.wait()
+        timestamp("All Server Workers", "Initialized")
+        timestamp("Scheduler", "Initializing")
+
+        timestamp("Scheduler", "Initialized")
