4dcb65ba6c2e0d145898ec0244bfbaf6987164fa
Raphael-Hao <raphaelhao@outlook.com>
2021-04-05 19:25:16 +0800

    update result for qos experiment

--- nvidia_utils.sh
+++ nvidia_utils.sh
@@ -44,7 +44,6 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-MIG_1=
 MIG_2=("MIG-GPU-95be3bb0-41c3-8f7b-47af-20c3799bcf22/3/0"
   "MIG-GPU-95be3bb0-41c3-8f7b-47af-20c3799bcf22/5/0")
 MIG_4=("MIG-GPU-95be3bb0-41c3-8f7b-47af-20c3799bcf22/7/0"
@@ -62,6 +61,7 @@ if [ "$TASK" = "MPS" ]; then
         export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log-0
         export CUDA_VISIBLE_DEVICES=MIG-GPU-95be3bb0-41c3-8f7b-47af-20c3799bcf22/2/0
         nvidia-cuda-mps-control -d
+        echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} started"
       elif [ "$MIG_CNT" = 2 ]; then
         for ((i = 0; i < "$MIG_CNT"; i++)); do
           echo "${MIG_2[${i}]}"
@@ -69,6 +69,7 @@ if [ "$TASK" = "MPS" ]; then
           export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log-"${i}"
           export CUDA_VISIBLE_DEVICES="${MIG_2[${i}]}"
           nvidia-cuda-mps-control -d
+          echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} started"
         done
       elif [ "$MIG_CNT" = 4 ]; then
         for ((i = 0; i < "$MIG_CNT"; i++)); do
@@ -77,6 +78,7 @@ if [ "$TASK" = "MPS" ]; then
           export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log-"${i}"
           export CUDA_VISIBLE_DEVICES="${MIG_4[${i}]}"
           nvidia-cuda-mps-control -d
+          echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} started"
         done
       fi
     else
@@ -84,6 +86,7 @@ if [ "$TASK" = "MPS" ]; then
       export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
       export CUDA_VISIBLE_DEVICES=GPU-95be3bb0-41c3-8f7b-47af-20c3799bcf22
       nvidia-cuda-mps-control -d
+      echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} started"
     fi
   elif [ "$JOB" = "stop" ]; then
     echo "Stopping the mps server"
@@ -93,6 +96,7 @@ if [ "$TASK" = "MPS" ]; then
         export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log-0
         export CUDA_VISIBLE_DEVICES=MIG-GPU-95be3bb0-41c3-8f7b-47af-20c3799bcf22/2/0
         echo quit | nvidia-cuda-mps-control
+        echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} stopped"
       elif [ "$MIG_CNT" = 2 ]; then
         for ((i = 0; i < "$MIG_CNT"; i++)); do
           echo "${MIG_2[${i}]}"
@@ -100,6 +104,7 @@ if [ "$TASK" = "MPS" ]; then
           export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log-"${i}"
           export CUDA_VISIBLE_DEVICES="${MIG_2[${i}]}"
           echo quit | nvidia-cuda-mps-control
+          echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} stopped"
         done
       elif [ "$MIG_CNT" = 4 ]; then
         for ((i = 0; i < "$MIG_CNT"; i++)); do
@@ -108,13 +113,16 @@ if [ "$TASK" = "MPS" ]; then
           export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log-"${i}"
           export CUDA_VISIBLE_DEVICES="${MIG_4[${i}]}"
           echo quit | nvidia-cuda-mps-control
+          echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} stopped"
         done
       fi
     else
+
       export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
       export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
       export CUDA_VISIBLE_DEVICES=GPU-95be3bb0-41c3-8f7b-47af-20c3799bcf22
       echo quit | nvidia-cuda-mps-control
+      echo "MPS server at  INSTANCE ${CUDA_VISIBLE_DEVICES} stopped"
     fi
   fi
 elif [ "$TASK" = "MIG" ]; then
