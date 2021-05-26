8822de5c5f5282157092e3f2c02d074f2a36243f
Raphael-Hao <raphaelhao@outlook.com>
2021-05-26 13:34:54 +0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,10 +4,5 @@
 # \brief: setup the environment
 # Author: raphael hao
 
-mkdir -p ~/.local
-chmod +x Anaconda3.sh
-bash ./Anaconda3.sh -p "$HOME"/.local/anaconda3
-pip3 install torch-1.7.1+cu110-cp38-cp38-linux_x86_64.whl torchvision-0.8.2+cu110-cp38-cp38-linux_x86_64.whl
-pip3 install -r requirements.txt
 GPU_CNT=$(nvidia-smi -L | wc -l)
 ./nvidia_utils.sh --mps 1 --ngpu "$GPU_CNT"
